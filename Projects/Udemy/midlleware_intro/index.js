4250cbe0f102782f0f70f29f8cef86cdeeb28659
ShauryaMalhan <seemashaurya13@gmail.com>
2024-01-25 03:58:45 +0530

    Added error handling

--- Projects/Udemy/midlleware_intro/index.js
+++ Projects/Udemy/midlleware_intro/index.js
@@ -9,6 +9,14 @@ app.use((req, res, next) => {
     return next();
 })
 
+const verifyPassword = (req, res, next) => {
+    const { password } = req.query;
+    if(password === 'swasti'){
+        next();
+    }
+    throw new Error('Password required!!');
+};
+
 app.use('/dogs' ,(req, res, next) => {
     console.log("I love dog!");
     next();
@@ -24,9 +32,17 @@ app.get('/', (req, res) => {
     res.send('Home page');
 });
 
+app.get('/secrets', verifyPassword, (req, res) => {
+    res.send('Home page');
+});
+
 app.get('/dogs', (req, res) => {
     console.log(`Request Date: ${req.requestTime}`);
     res.send('Woof Woof!!!');
+});
+
+app.get('/error', (req, res) => {
+    chicken.fly();
 });
 
 app.use((req, res, next) => {
